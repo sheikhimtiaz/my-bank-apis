b9108249578478863c6d6aed566bdfd0fff1d047
sheikhimtiaz <imticorei54@gmail.com>
2021-04-12 22:49:22 +0600

    swagger added

--- AccountService/src/main/java/com/mybank/accountservice/AccountServiceApplication.java
+++ AccountService/src/main/java/com/mybank/accountservice/AccountServiceApplication.java
@@ -3,10 +3,12 @@ package com.mybank.accountservice;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Mapper()
 @SpringBootApplication
+@EnableSwagger2
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
--- AccountService/src/main/java/com/mybank/accountservice/swagger/ConfigSwagger.java
+++ AccountService/src/main/java/com/mybank/accountservice/swagger/ConfigSwagger.java
@@ -1,22 +1,30 @@
-//package com.mybank.accountservice.swagger;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//@Configuration
-//@EnableSwagger2
-//public class ConfigSwagger {
-//    @Bean
-//    public Docket api(){
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
-//                .build();
-//    }
-//}
+package com.mybank.accountservice.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class ConfigSwagger {
+    private static final String[] AUTH_LIST = {
+            // -- swagger ui
+            "**/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mybank."))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
